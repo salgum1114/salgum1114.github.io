72bbcb1cb2341fcde5cefb4488c4e59743e20961
sgoh <sgoh@nkia.co.kr>
2019-04-23 12:52:54 +0900

    헤더 zIndex 수정

--- src/containers/Container.tsx
+++ src/containers/Container.tsx
@@ -13,6 +13,7 @@ const styles: CSSMapper = {
     header: {
         padding: 0,
         boxShadow: '0px 0px 10px -2px rgba(0, 0, 0, 0.75)',
+        zIndex: 1000,
     },
     content: {
         overflow: 'auto',
