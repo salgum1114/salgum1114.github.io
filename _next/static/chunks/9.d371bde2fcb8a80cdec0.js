576c9aec1bfd5c5dfee6c8ef3b2ece8e36198a0d
salgum1114 <salgum1112@gmail.com>
2019-11-28 16:29:35 +0900

    Posted docs

--- _next/static/chunks/9.d371bde2fcb8a80cdec0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{710:function(a){a.exports={salgum1114:{name:"salgum1114",title:"Sung Gyun Oh",avatar:"/static/images/authors/salgum1114.png",bio:"Hello world!"}}}}]);
